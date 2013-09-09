3ed08280dd131e2bdd93789b2ec61c9f022fa808
Siwaÿll <sanath.labs@gmail.com>
2013-09-09 10:07:01 +0200

    initial

---
+++ clock.php
@@ -0,0 +1,24 @@
+<?php
+
+$date = date('Y.m.d');
+$fileName = $date . '.csv';
+
+file_put_contents($fileName, '', FILE_APPEND);
+
+do {
+    $start = new DateTime();
+    file_put_contents($fileName, "\r\n" . $start->format('H:i') . ';', FILE_APPEND);
+    $choice = trim(fgets(STDIN));
+    $end = new DateTime();
+    $line = $end->format('H:i') . ';';
+    $diff = $start->diff($end);
+
+    $line .= $diff->format('%hh %im') . ';' . $choice;
+
+    file_put_contents($fileName, $line, FILE_APPEND);
+
+    echo $diff->format('%hh %im') . "\r\n";
+
+} while ($choice != 'q');
+
+
